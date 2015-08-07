b9c6c56287fb7ef258af84fe1dd26fe9ada31cd9
pione30 <g.pionelish30@gmail.com>
2015-08-07 11:30:44 +0900

    default .bashrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
