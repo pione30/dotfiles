c78549080b0d487e2d34a8f3c9f6bf2d455a269b
pione30 <g.pionelish30@gmail.com>
2018-06-13 11:51:10 +0900

    ignore .gitignore when ln -s

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for df in \.*; do
   # 通常のファイルのみに対してシンボリックリンクを作成する
-  if [ -f $df ]; then
+  if [ -f $df ] && [ $df != ".gitignore" ]; then
     ln -sf ~/dotfiles/$df ~/$df
     echo "ln -sf ~/dotfiles/$df ~/$df"
   fi
