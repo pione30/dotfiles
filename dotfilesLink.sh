4a9b562835ebc4a02bed9667f7f602155bcc21a3
pione30 <g.pionelish30@gmail.com>
2019-03-19 17:47:18 +0900

    use -v instead of echo

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,8 +2,7 @@
 for df in \.*; do
   # 通常のファイルのみに対してシンボリックリンクを作成する
   if [ -f $df ] && [ $df != ".gitignore" ]; then
-    ln -sf ~/dotfiles/$df ~/$df
-    echo "ln -sf ~/dotfiles/$df ~/$df"
+    ln -svf ~/dotfiles/$df ~/$df
   fi
 done
 
