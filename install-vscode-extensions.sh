55b6e54c36f1ac6c7852eef7572534227fc36ffe
pione30 <g.pionelish30@gmail.com>
2022-05-20 14:33:35 +0900

    rust-analyzer is now under rust-lang

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -4,5 +4,4 @@ code --install-extension eamodio.gitlens
 code --install-extension oderwat.indent-rainbow
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension esbenp.prettier-vscode
-code --install-extension matklad.rust-analyzer
-
+code --install-extension rust-lang.rust-analyzer
